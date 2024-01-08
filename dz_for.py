2896a4fd774b4a12ad0a7f6be2618724f7e42408
testP <d.chernyagin@gmail.com>
2024-01-08 23:16:41 +0400

    Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"

---
+++ dz_for.py
@@ -0,0 +1,12 @@
+# Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"
+
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for car in cars:
+    print("Я езже на автомобиле марки " + car)
+
+car_count = 0
+i = 0
+while i <= 2:
+    i += 1
+    car_count += 10
+    print(car_count)
