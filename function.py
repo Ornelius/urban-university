4663f1bdb41ba2b71f3cf7ed345906b471709b62
testP <d.chernyagin@gmail.com>
2024-01-08 23:45:16 +0400

    2023/10/01 00:00|Домашняя работа по уроку "Функции в Python.Функция с параметром"

---
+++ function.py
@@ -0,0 +1,10 @@
+# 2023/10/01 00:00|Домашняя работа по уроку "Функции в Python.Функция с параметром"
+
+def print_params(param1):
+    print(param1)
+
+
+param = 'r'
+print_params(param)
+param = 2
+print_params(param)
