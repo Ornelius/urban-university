105c07f361cbaf50f50f326c749263c338ee4b83
testP <d.chernyagin@gmail.com>
2024-01-09 21:27:30 +0400

    2023/10/07 00:00|Самостоятельная работа по уроку "Распаковка параметров и параметры функции"

--- dz_for.py
+++ dz_for.py
@@ -4,9 +4,14 @@ cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 for car in cars:
     print("Я езже на автомобиле марки " + car)
 
+
+i = 1
 car_count = 0
-i = 0
-while i <= 2:
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for car in cars:
+    print("Я езжу на автомобиле марки " + car)
+    if i >= 2:
+        car_count += 10
+    print("car_count: ", car_count)
     i += 1
-    car_count += 10
-    print(car_count)
+
