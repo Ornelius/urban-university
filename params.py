03ac0f607ac1c0bdae2a3b84585502692df9698c
testP <d.chernyagin@gmail.com>
2024-01-09 10:54:12 +0400

    2023/10/07 00:00|Самостоятельная работа по уроку "Распаковка параметров и параметры функции"

--- params.py
+++ params.py
@@ -1,4 +1,6 @@
 # 2023/10/07 00:00|Самостоятельная работа по уроку "Распаковка параметров и параметры функции"
+from test import a
+
 
 def print_params(a=1, b="string", c=True):
     print('a: ', a, " b: ", b, " c: ", c)
@@ -6,6 +8,16 @@ def print_params(a=1, b="string", c=True):
 
 print_params()
 print_params(3,4,5)
-print_params(3,4,5, 6) # не работает
+#print_params(3,4,5, 6) # не работает
 print_params(b = 25)
 print_params(c = [1,2,3])
+
+valuse_list = [1,"string", True]
+values_dict = {'a': 1, 'b': "string", 'c':True}
+
+
+print_params(*valuse_list)
+print_params(**values_dict)
+
+values_list_2 = [True, 6]
+print_params(*values_list_2, 42)
