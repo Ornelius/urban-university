888f7dd4651c3bd79e9d3b2151058fe014d20dd4
testP <d.chernyagin@gmail.com>
2024-01-23 08:00:36 +0400

    2023/10/14 00:00|Модули и пакеты

--- example.py
+++ example.py
@@ -1,4 +1,8 @@
+#2023/10/14 00:00|Модули и пакеты
+
+
 from example_lib import hello_world_1 as hw1, hello_world_2 as hw2
 
 hw1()
+
 hw2()
--- example_lib.py
+++ example_lib.py
@@ -1,3 +1,4 @@
+#2023/10/14 00:00|Модули и пакеты
 def hello_world_1():
     print("Hello world 1")
 
